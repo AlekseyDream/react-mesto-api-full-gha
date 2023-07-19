0cfefc2b4af7bd316c397c65c9cec395b58c19d4
AlekseyDream <u550@list.ru>
2023-07-19 09:13:52 +0300

    gg

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 const router = require('./routes');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUser, login } = require('./controllers/users');
 const { signinValidate, signupValidate } = require('./middlewares/validation');
 
@@ -11,11 +12,18 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signup', signupValidate, createUser);
 app.post('/signin', signinValidate, login);
 app.use(auth);
 app.use(router);
-app.use(helmet());
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
